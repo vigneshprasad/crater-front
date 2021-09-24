bff434922df355dc4910a16c1ff04d6bf01362a1
sanjeev40 <sanjeev@worknetwork.in>
2021-09-24 18:20:28 +0530

    Initialize segment to record page views

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import * as snippet from "@segment/snippet";
 import { ServerStyleSheet } from "styled-components";
 
 import Document, {
@@ -8,6 +9,8 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+
+const SEGMENT_KEY = process.env.SEGMENT_KEY as string;
 
 export default class AppDocument extends Document {
   static async getInitialProps(
@@ -38,11 +41,25 @@ export default class AppDocument extends Document {
     }
   }
 
+  renderSnippet() {
+    const opts = {
+      apiKey: SEGMENT_KEY,
+      page: true,
+    };
+
+    if (process.env.NODE_ENV === "development") {
+      return snippet.max(opts);
+    }
+
+    return snippet.min(opts);
+  }
+
   render(): JSX.Element {
     return (
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
         </Head>
         <body>
           <Main />
