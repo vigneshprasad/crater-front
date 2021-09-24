2ee2df58a6d0ac4feab82173a7a549b1ff0f1fc7
Abhishek R <abhishek@worknetwork.in>
2021-09-25 00:55:19 +0530

    undefined chek for window

--- lib/common/utils/segment/index.ts
+++ lib/common/utils/segment/index.ts
@@ -5,12 +5,14 @@ interface ISegmentUtils {
 }
 
 const sendDataToSegment = (segmentData: ISegmentUtils): void => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  window.analytics.track(segmentData.actionName, {
-    datetime: segmentData.datetime,
-    username: segmentData.username,
-  });
+  if (typeof window !== "undefined") {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.analytics.track(segmentData.actionName, {
+      datetime: segmentData.datetime,
+      username: segmentData.username,
+    });
+  }
 };
 
 export default sendDataToSegment;
