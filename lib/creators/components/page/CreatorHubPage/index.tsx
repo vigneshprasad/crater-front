d826892071b8ea2552e5899a90ca14173d069089
sanjeev40 <sanjeev@worknetwork.in>
2021-10-12 15:30:13 +0530

    UI for creatorhub page with streams tab

---
+++ lib/creators/components/page/CreatorHubPage/index.tsx
@@ -0,0 +1,33 @@
+import { NextSeoProps } from "next-seo";
+import { PropsWithChildren } from "react";
+import { useTheme } from "styled-components";
+
+import { Box } from "@/common/components/atoms";
+import BaseLayout from "@/common/components/layouts/BaseLayout";
+import AsideNav from "@/common/components/objects/AsideNav";
+import Page from "@/common/components/objects/Page";
+
+type IProps = PropsWithChildren<{
+  selectedTab: string;
+}>;
+
+export default function CreatorHubPage({ children }: IProps) {
+  const seo: NextSeoProps = {
+    title: "Crater Club",
+    description: "Creater Hub",
+  };
+  const { space } = useTheme();
+
+  return (
+    <Page seo={seo}>
+      <BaseLayout
+        overflowY="auto"
+        pb={space.l}
+        aside={<AsideNav activeTab="creatorhub" />}
+      >
+        <Box h={240} bg="#fff" />
+        {children}
+      </BaseLayout>
+    </Page>
+  );
+}
