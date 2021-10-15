19366e0190067a98b0a1ed3d5124d03be7da0c11
sanjeev40 <sanjeev@worknetwork.in>
2021-10-15 16:59:29 +0530

    Add DateTimeInput component

---
+++ lib/common/components/objects/DateTimeInput/index.tsx
@@ -0,0 +1,49 @@
+import { forwardRef, useMemo } from "react";
+import styled, { useTheme } from "styled-components";
+
+import { InputContainer, StyledInput, InputProps, Text } from "../../atoms";
+
+type IProps = InputProps;
+
+const StyledPicker = styled(StyledInput)`
+  &::-webkit-calendar-picker-indicator {
+    filter: invert(1);
+  }
+`;
+
+function DateTimeInputWithRef({
+  type = "date",
+  prefixElement,
+  inputRef,
+  error,
+  ...rest
+}: IProps & {
+  inputRef: React.ForwardedRef<HTMLInputElement>;
+}): JSX.Element {
+  const { space, colors } = useTheme();
+
+  const border = useMemo(() => {
+    return `2px solid ${error ? colors.error : "transparent"}`;
+  }, [error, colors]);
+  return (
+    <>
+      <InputContainer border={border}>
+        {prefixElement && prefixElement}
+        <StyledPicker ref={inputRef} type={type} {...rest} />
+      </InputContainer>
+      {error && (
+        <Text py={space.xxs} color={colors.error} textStyle="error">
+          {error}
+        </Text>
+      )}
+    </>
+  );
+}
+
+const DateTimeInput = forwardRef<HTMLInputElement, IProps>((props, ref) => (
+  <DateTimeInputWithRef {...props} inputRef={ref} />
+));
+
+DateTimeInput.displayName = "DateTimeInput";
+
+export default DateTimeInput;
