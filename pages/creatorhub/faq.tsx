0287841786e4c190ef8671fb6351fa1800f2b2b6
sanjeev40 <sanjeev@worknetwork.in>
2021-11-16 15:53:01 +0530

    Use dynamic imports

--- pages/creatorhub/faq.tsx
+++ pages/creatorhub/faq.tsx
@@ -1,8 +1,13 @@
+import dynamic from "next/dynamic";
+
 import useAuth from "@/auth/context/AuthContext";
 import Spinner from "@/common/components/atoms/Spiner";
-import CreatorHubFaqTab from "@/creators/components/objects/CreatorHubFaqTab";
 import CreatorHubPage from "@/creators/components/page/CreatorHubPage";
 import StaticCreatorHub from "@/creators/components/page/StaticCreatorHub";
+
+const CreatorHubFaqTab = dynamic(
+  () => import("@/creators/components/objects/CreatorHubFaqTab")
+);
 
 export default function CreatorHubFaq(): JSX.Element {
   const { user, profile, loading } = useAuth();
