58510aa4653fe183a9019874ba8a4b1341fee208
sanjeev40 <sanjeev@worknetwork.in>
2021-12-02 16:22:14 +0530

    Add creator follower table component

---
+++ lib/creators/components/objects/CreatorFollowerTable/index.tsx
@@ -0,0 +1,113 @@
+import { useCallback } from "react";
+import { useTheme } from "styled-components";
+
+import { Box } from "@/common/components/atoms";
+import Spinner from "@/common/components/atoms/Spiner";
+import { Follower } from "@/community/types/community";
+
+interface IProps {
+  data?: Follower[];
+  loading: boolean;
+  error?: unknown;
+}
+
+export default function CreatorFollowerTable({ data }: IProps): JSX.Element {
+  const { colors } = useTheme();
+
+  const columns = [
+    {
+      Header: "Name",
+      id: "name",
+      accessor: "profile_detail.name",
+    },
+    {
+      Header: "Email",
+      id: "email",
+      accessor: "profile_detail.email",
+    },
+    {
+      Header: "Phone Number",
+      id: "phone",
+      accessor: "profile_detail.phone_number",
+    },
+  ];
+
+  const getValue = useCallback((accessor: string, data: Follower) => {
+    const split = accessor.split(".");
+
+    return split.reduce((prev, curr) => {
+      return prev[curr];
+    }, data);
+  }, []);
+
+  return (
+    <Box>
+      <table
+        style={{
+          width: "80%",
+          margin: "0 auto",
+          tableLayout: "fixed",
+        }}
+      >
+        <thead
+          style={{
+            textAlign: "left",
+          }}
+        >
+          <tr>
+            {columns.map((column) => (
+              <th
+                style={{
+                  width: "50px",
+                  padding: "12px",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                  fontWeight: 600,
+                  border: `1px solid ${colors.black[6]}`,
+                  backgroundColor: colors.black[1],
+                }}
+                key={column.id}
+              >
+                {column.Header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody
+          style={{
+            textAlign: "left",
+          }}
+        >
+          {(() => {
+            if (!data) {
+              return <Spinner />;
+            }
+
+            return data.map((row) => {
+              return (
+                <tr key={row.id}>
+                  {columns.map((column) => {
+                    return (
+                      <td
+                        style={{
+                          padding: "14px",
+                          fontSize: "14px",
+                          fontWeight: 300,
+                          border: `1px solid ${colors.black[6]}`,
+                        }}
+                        key={column.id}
+                      >
+                        {getValue(column.accessor, row)}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            });
+          })()}
+        </tbody>
+      </table>
+    </Box>
+  );
+}
