0c6c86420e722c27fe945ee846ecac56af920de1
sanjeev40 <sanjeev@worknetwork.in>
2022-02-01 14:47:47 +0530

    Remove redundant useMemo

--- lib/community/components/pages/SessionPage/index.tsx
+++ lib/community/components/pages/SessionPage/index.tsx
@@ -127,10 +127,6 @@ export default function SessionPage({ id }: IProps): JSX.Element {
       : null
   );
 
-  const series = useMemo(() => {
-    return seriesData;
-  }, [seriesData]);
-
   const postSeriesRequest = useCallback(async (): Promise<void> => {
     if (webinar && webinar?.series !== null && seriesData) {
       if (webinarRequest?.status !== RequestStatus.accepted) {
@@ -486,7 +482,7 @@ export default function SessionPage({ id }: IProps): JSX.Element {
               gridTemplateColumns={["1fr", "repeat(4, 1fr)"]}
               gridGap={space.s}
             >
-              {series?.groups_detail_list.map((stream) => {
+              {seriesData?.groups_detail_list.map((stream) => {
                 if (stream.id !== webinar.id) {
                   return <StreamCard stream={stream} key={stream.id} />;
                 }
