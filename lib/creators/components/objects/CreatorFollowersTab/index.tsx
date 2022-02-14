99567244d5978650d187895ce27e131f9b03cc7a
sanjeev40 <sanjeev@worknetwork.in>
2022-02-14 10:54:17 +0530

    Show analytics in creatorhub

--- lib/creators/components/objects/CreatorFollowersTab/index.tsx
+++ lib/creators/components/objects/CreatorFollowersTab/index.tsx
@@ -1,11 +1,42 @@
 import { AxiosError } from "axios";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  PieChart,
+  Pie,
+} from "recharts";
 import { useTheme } from "styled-components";
 
 import API from "@/common/api";
-import { Box } from "@/common/components/atoms";
+import {
+  Avatar,
+  Box,
+  Card,
+  Flex,
+  Grid,
+  Image,
+  Text,
+} from "@/common/components/atoms";
+import Spinner from "@/common/components/atoms/Spiner";
+import DataTable from "@/common/components/objects/DataTable";
+import { Column } from "@/common/components/objects/DataTable/types";
 import { API_URL_CONSTANTS } from "@/common/constants/url.constants";
+import { useAverageEngagement } from "@/creators/context/AverageEngagement";
+import { useClubMembersCount } from "@/creators/context/ClubMembersCount";
+import { useClubMembersGrowth } from "@/creators/context/ClubMembersGrowth";
+import { useComparativeEngagement } from "@/creators/context/ComparativeEngagement";
+import { useConversionFunnel } from "@/creators/context/ConversionFunnel";
 import useCreatorFollowers from "@/creators/context/CreatorFollowerContext";
+import { useTopStreams } from "@/creators/context/CreatorTopStreams";
+import { useFollowerGrowth } from "@/creators/context/FollowerGrowth";
+import { useTopCreators } from "@/creators/context/TopCreators";
+import { useTrafficSourceTypes } from "@/creators/context/TrafficSourceTypes";
+import { TopCreators } from "@/creators/types/creator";
+import { TopStreams } from "@/creators/types/stream";
 
 import CreatorFollowerTable from "../CreatorFollowerTable";
 
@@ -14,7 +45,25 @@ export default function CreatorFollowersTab(): JSX.Element {
   const ref = useRef<HTMLAnchorElement>(null);
   const { followers, setPage, loading, currentPage, pageCount } =
     useCreatorFollowers();
-  const { space } = useTheme();
+  const { space, colors } = useTheme();
+  const { clubMembersCount, loading: clubMembersCountLoading } =
+    useClubMembersCount();
+  const { followerGrowth, loading: followerGrowthLoading } =
+    useFollowerGrowth();
+  const { averageEngagement, loading: averageEngagementLoading } =
+    useAverageEngagement();
+  const { comparativeEngagement, loading: comparativeEngagementLoading } =
+    useComparativeEngagement();
+  const { topStreams, loading: topStreamsLoading } = useTopStreams();
+  const { topCreators, loading: topCreatorsLoading } = useTopCreators();
+  const { clubMembersGrowth, loading: clubMembersGrowthLoading } =
+    useClubMembersGrowth();
+  const { trafficSourceTypes, loading: trafficSourceTypesLoading } =
+    useTrafficSourceTypes();
+  const { conversionFunnel, loading: conversionFunnelLoading } =
+    useConversionFunnel();
+
+  console.log(trafficSourceTypes);
 
   function triggerFileDownload(response: string): void {
     if (ref.current) {
@@ -37,22 +86,318 @@ export default function CreatorFollowersTab(): JSX.Element {
     }
   }
 
+  const topStreamsColumns = useMemo<Column<TopStreams>[]>(() => {
+    return [
+      {
+        label: "Stream",
+        key: "stream",
+        valueGetter: (obj) => {
+          return (
+            <Flex flexDirection="row" gridGap={space.s} alignItems="center">
+              <Box w={130}>
+                {obj.topic_image && (
+                  <Image
+                    objectFit="cover"
+                    layout="fill"
+                    src={obj.topic_image}
+                    alt={obj.topic_title}
+                  />
+                )}
+              </Box>
+
+              <Flex flexDirection="column" gridGap={space.xxs}>
+                <Text>{obj.topic_title}</Text>
+                <Text>{obj.start}</Text>
+              </Flex>
+            </Flex>
+          );
+        },
+      },
+      {
+        label: "RSVP",
+        key: "rsvp",
+        valueGetter: (obj) => obj.rsvp_count,
+      },
+      {
+        label: "Messages",
+        key: "messages",
+        valueGetter: (obj) => obj.messages_count,
+      },
+    ];
+  }, [space]);
+
+  const topCreatorsColumns = useMemo<Column<TopCreators>[]>(() => {
+    return [
+      {
+        label: "Creator",
+        key: "creator",
+        valueGetter: (obj) => {
+          return (
+            <Flex flexDirection="row" gridGap={space.s} alignItems="center">
+              {obj.creator_image && (
+                <Avatar
+                  size={56}
+                  alt={obj.creator_name || ""}
+                  image={obj?.creator_image}
+                />
+              )}
+
+              <Text>{obj.creator_name}</Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        label: "Followers",
+        key: "followers",
+        valueGetter: (obj) => obj.follower_count,
+      },
+      {
+        label: "Top Stream",
+        key: "topStream",
+        valueGetter: (obj) => obj.stream_topic,
+      },
+    ];
+  }, [space]);
+
   return (
-    <Box px={[0, space.xs]} py={space.xxs} overflowX="auto">
-      <a
-        ref={ref}
-        href={href}
-        style={{ display: "none" }}
-        download="followers.csv"
-      />
-      <CreatorFollowerTable
-        pageCount={pageCount}
-        currentPage={currentPage}
-        loading={loading}
-        data={followers}
-        onPressDownloadCSV={handleExportCsvBtnClick}
-        setPage={setPage}
-      />
+    <Box px={[0, space.l]} py={space.xxs} overflowX="auto">
+      <Grid gridAutoFlow="row" gridGap={space.s}>
+        <Card containerProps={{ px: 0, py: 0 }}>
+          <Grid gridGap={space.s}>
+            <Grid
+              gridAutoFlow="column"
+              gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+            >
+              <Box
+                w="100%"
+                textAlign="center"
+                border={`1px solid ${colors.black[3]}`}
+                p={space.xxs}
+              >
+                <Text color={colors.accent}>My Club</Text>
+                {clubMembersCountLoading ? (
+                  <Spinner m="0 auto" />
+                ) : (
+                  <Flex
+                    m="0 auto"
+                    gridGap={space.xxxs}
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Text py={space.xxxs} textStyle="headline3">
+                      {clubMembersCount}
+                    </Text>
+                    <Text>Members</Text>
+                  </Flex>
+                )}
+              </Box>
+              <Box
+                w="100%"
+                textAlign="center"
+                border={`1px solid ${colors.black[3]}`}
+                p={space.xxs}
+              >
+                <Text color={colors.accent}>Growth this month</Text>
+                {followerGrowthLoading ? (
+                  <Spinner m="0 auto" />
+                ) : (
+                  <Text py={space.xxxs} textStyle="headline3">
+                    {followerGrowth}%
+                  </Text>
+                )}
+              </Box>
+              <Box
+                w="100%"
+                textAlign="center"
+                border={`1px solid ${colors.black[3]}`}
+                p={space.xxs}
+              >
+                <Text color={colors.accent}>Users brought in by Crater</Text>
+                <Text py={space.xxxs} textStyle="headline3">
+                  90%
+                </Text>
+              </Box>
+              <Box
+                w="100%"
+                textAlign="center"
+                border={`1px solid ${colors.black[3]}`}
+                p={space.xxs}
+              >
+                <Text color={colors.accent}>Average Engagement</Text>
+                {averageEngagementLoading ? (
+                  <Spinner m="0 auto" />
+                ) : (
+                  <Flex
+                    m="0 auto"
+                    gridGap={space.xxxs}
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Text py={space.xxxs} textStyle="headline3">
+                      {averageEngagement}
+                    </Text>
+                    <Text>Messages</Text>
+                  </Flex>
+                )}
+              </Box>
+              <Box
+                w="100%"
+                textAlign="center"
+                border={`1px solid ${colors.black[3]}`}
+                p={space.xxs}
+              >
+                <Text color={colors.accent}>Comparative Engagement</Text>
+                {comparativeEngagementLoading ? (
+                  <Spinner m="0 auto" />
+                ) : (
+                  <Text py={space.xxxs} textStyle="headline3">
+                    {comparativeEngagement}%
+                  </Text>
+                )}
+              </Box>
+            </Grid>
+
+            <Box px={space.s}>
+              {clubMembersGrowthLoading ? (
+                <Spinner m="0 auto" />
+              ) : (
+                <LineChart width={1500} height={200} data={clubMembersGrowth}>
+                  <Line
+                    type="monotone"
+                    dataKey="follower_count"
+                    stroke="#8884d8"
+                  />
+                  <CartesianGrid stroke={colors.black[1]} vertical={false} />
+                  <XAxis
+                    dataKey="followed_at_date"
+                    interval="preserveStartEnd"
+                  />
+                  <YAxis orientation="right" allowDecimals={false} />
+                </LineChart>
+              )}
+            </Box>
+          </Grid>
+        </Card>
+
+        <Grid
+          gridAutoFlow="column"
+          gridTemplateColumns="1fr 1fr"
+          gridGap={space.s}
+        >
+          <Card containerProps={{ p: 0, textAlign: "center" }}>
+            <Text py={space.xxs}>Traffic Source Types</Text>
+
+            {trafficSourceTypesLoading ? (
+              <Spinner m="0 auto" />
+            ) : (
+              <Grid
+                gridAutoFlow="column"
+                gridGap={space.xxs}
+                gridTemplateColumns="1fr 1fr"
+              >
+                <PieChart width={400} height={400}>
+                  <Pie
+                    data={trafficSourceTypes}
+                    dataKey="count"
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={70}
+                    outerRadius={90}
+                    paddingAngle={5}
+                    fill={colors.accent}
+                    label
+                  />
+                </PieChart>
+
+                <Grid
+                  m="auto 0"
+                  gridAutoFlow="row"
+                  alignItems="center"
+                  gridGap={space.xs}
+                >
+                  {trafficSourceTypes &&
+                    trafficSourceTypes.map((obj) => (
+                      <Grid
+                        gridAutoFlow="column"
+                        gridGap={space.xs}
+                        key={obj.source_name}
+                      >
+                        <Text justifySelf="start">{obj.source_name}</Text>
+                        <Text justifySelf="end">{obj.count}</Text>
+                      </Grid>
+                    ))}
+                </Grid>
+              </Grid>
+            )}
+          </Card>
+
+          <Card containerProps={{ p: 0, textAlign: "center" }}>
+            <Text py={space.xxs}>Traffic Source Types</Text>
+            {conversionFunnelLoading ? (
+              <Spinner />
+            ) : (
+              <Grid my={space.l} gridAutoFlow="row" gridGap={space.xxs}>
+                <Text>Total RSVPs</Text>
+                <Text>{conversionFunnel?.rsvp_count}</Text>
+
+                <Text>Total Subscribers</Text>
+                <Text>{conversionFunnel?.subscriber_count}</Text>
+
+                <Text>Total Recurring Users</Text>
+                <Text>{conversionFunnel?.recurring_user_count}</Text>
+              </Grid>
+            )}
+          </Card>
+        </Grid>
+
+        <Grid
+          gridAutoFlow="column"
+          gridTemplateColumns="1fr 1fr"
+          gridGap={space.s}
+        >
+          <Card containerProps={{ p: 0, textAlign: "center" }}>
+            <Text py={space.xxs}>Your Top Performing Streams</Text>
+
+            {topStreamsLoading ? (
+              <Spinner m="0 auto" />
+            ) : (
+              <DataTable columns={topStreamsColumns} data={topStreams} />
+            )}
+          </Card>
+
+          <Card containerProps={{ p: 0, textAlign: "center" }}>
+            <Grid gridAutoFlow="row" gridTemplateRows="max-content 1fr">
+              <Text py={space.xxs} justifySelf="center">
+                Comparative Ranking
+              </Text>
+
+              {topCreatorsLoading ? (
+                <Spinner m="0 auto" />
+              ) : (
+                <DataTable columns={topCreatorsColumns} data={topCreators} />
+              )}
+            </Grid>
+          </Card>
+        </Grid>
+
+        <Box>
+          <a
+            ref={ref}
+            href={href}
+            style={{ display: "none" }}
+            download="followers.csv"
+          />
+          <CreatorFollowerTable
+            pageCount={pageCount}
+            currentPage={currentPage}
+            loading={loading}
+            data={followers}
+            onPressDownloadCSV={handleExportCsvBtnClick}
+            setPage={setPage}
+          />
+        </Box>
+      </Grid>
     </Box>
   );
 }
