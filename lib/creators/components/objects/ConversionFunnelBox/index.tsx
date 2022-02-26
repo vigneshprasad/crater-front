8819fbcf998e4bddb6e3c3ee1cfbe903e0369328
sanjeev40 <sanjeev@worknetwork.in>
2022-02-26 20:22:39 +0530

    Update conversion funnel ui

--- lib/creators/components/objects/ConversionFunnelBox/index.tsx
+++ lib/creators/components/objects/ConversionFunnelBox/index.tsx
@@ -1,56 +1,67 @@
-import {
-  Funnel,
-  FunnelChart,
-  LabelList,
-  ResponsiveContainer,
-  Tooltip,
-} from "recharts";
 import { useTheme } from "styled-components";
 
-import { Box, Card, Shimmer, Text } from "@/common/components/atoms";
+import {
+  Box,
+  Card,
+  CardProps,
+  Flex,
+  Shimmer,
+  Text,
+} from "@/common/components/atoms";
+import colors from "@/common/theme/colors";
 import { ConversionFunnel } from "@/creators/types/creator";
 
-interface IProps {
+interface IProps extends CardProps {
   conversionFunnelData?: ConversionFunnel[];
 }
 
 export default function ConversionFunnelBox({
   conversionFunnelData,
+  ...rest
 }: IProps): JSX.Element {
-  const { space, colors } = useTheme();
+  const { space } = useTheme();
 
   if (conversionFunnelData === undefined) {
     return <Shimmer w="100%" h="100%" />;
   }
 
   return (
-    <Card containerProps={{ px: space.xs, py: space.xs }}>
+    <Card containerProps={{ px: space.xs, py: space.xs }} {...rest}>
       <Text pb={space.xs} textStyle="headline5">
         Your Conversion Funnel
       </Text>
 
-      <Box h={240}>
-        <ResponsiveContainer width="100%" height="100%">
-          <FunnelChart>
-            <Tooltip />
-            <Funnel
-              dataKey="count"
-              data={conversionFunnelData}
-              fill={colors.accent}
-              isAnimationActive
+      <Flex flexDirection="column" alignItems="center" gridGap={space.xs}>
+        {conversionFunnelData.map((obj, index) => {
+          return (
+            <Flex
+              flexDirection="column"
+              w={[
+                450 / (index / 4.5 + 1),
+                450 / (index / 4.5 + 1),
+                450 / (index / 4.5 + 1),
+              ]}
+              h={[80, 80, 80]}
+              key={obj.name}
+              alignItems="center"
+              justifyContent="center"
+              textAlign="center"
+              borderTop={[
+                `80px solid ${colors.black[0]}`,
+                `80px solid ${colors.black[0]}`,
+                `80px solid ${colors.black[0]}`,
+              ]}
+              borderLeft="25px solid transparent"
+              borderRight="25px solid transparent"
             >
-              <LabelList
-                position="insideTop"
-                offset={10}
-                fill={colors.white[0]}
-                stroke="none"
-                dataKey="name"
-                fontSize="1.3rem"
-              />
-            </Funnel>
-          </FunnelChart>
-        </ResponsiveContainer>
-      </Box>
+              <Box mt={["-80px", "-80px", "-80px"]}>
+                <Text textStyle="headline6">{obj.name}</Text>
+                <Text textStyle="headline6">{obj.count}</Text>
+              </Box>
+            </Flex>
+          );
+        })}
+      </Flex>
     </Card>
   );
 }
