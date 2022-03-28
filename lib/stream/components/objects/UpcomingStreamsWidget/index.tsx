eb5fd30489b923f99ccf0661910604c09744fb26
Abhishek R <abhishek@worknetwork.in>
2022-03-28 21:15:39 +0530

    Fixed transition issue

--- lib/stream/components/objects/UpcomingStreamsWidget/index.tsx
+++ lib/stream/components/objects/UpcomingStreamsWidget/index.tsx
@@ -78,8 +78,12 @@ export default function UpcomingStreamsWidget(): JSX.Element {
         variants={{
           closed: {
             transform: "translate(420px, 0)",
+            transitionEnd: {
+              display: "none",
+            },
           },
           opened: {
+            display: "block",
             transform: "translate(0px, 0)",
           },
         }}
