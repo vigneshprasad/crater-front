f66b23b0a36a7a1521d1edf2660a0e0f6676cb31
sanjeev40 <sanjeev@worknetwork.in>
2022-04-04 12:04:57 +0530

    Update referral summary section in modal

--- lib/community/components/objects/ReferralModal/index.tsx
+++ lib/community/components/objects/ReferralModal/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useTheme } from "styled-components";
 
 import {
@@ -7,13 +8,17 @@ import {
   Text,
   Flex,
   Grid,
+  Image,
 } from "@/common/components/atoms";
 import { useReferralSummary } from "@/tokens/context/ReferralSummaryContext";
+import { ReferralSummary } from "@/tokens/types/referrals";
 
 interface IProps {
   visible: boolean;
   onClose: () => void;
 }
+
+type ValueKeys = keyof ReferralSummary;
 
 export default function ReferralModal({
   visible,
@@ -22,18 +27,53 @@ export default function ReferralModal({
   const { space, colors } = useTheme();
   const { referralSummary } = useReferralSummary();
 
+  const referralSummaryData = useMemo<
+    {
+      title: string;
+      key: ValueKeys;
+      display: string;
+    }[]
+  >(() => {
+    return [
+      {
+        title: "Referrals who watched streams",
+        key: "total_referrals",
+        display: `${referralSummary?.total_referrals}`,
+      },
+      {
+        title: "Total Payable",
+        key: "total_payable",
+        display: `${referralSummary?.total_payable}`,
+      },
+      {
+        title: "Total Paid Out",
+        key: "paid_out",
+        display: `${referralSummary?.paid_out}`,
+      },
+      {
+        title: "Outstanding Payment",
+        key: "outstanding_payment",
+        display: `${referralSummary?.outstanding_payment}`,
+      },
+    ];
+  }, [referralSummary]);
+
   const steps = [
     {
       text: "Share the referral link with your friends",
+      image: "/images/img_journey_planet_1.png",
     },
     {
       text: "Your friend uses the link to sign up & watch their first stream",
+      image: "/images/img_journey_planet_2.png",
     },
     {
       text: "You get ₹100 when they watch their first 20 minutes of live content",
+      image: "/images/img_journey_planet_3.png",
     },
     {
       text: "Withdraw the money or use it at the auctions",
+      image: "/images/img_journey_planet_4.png",
     },
   ];
 
@@ -48,8 +88,7 @@ export default function ReferralModal({
       gridTemplateRows={["1fr 300px", "1fr"]}
       p={space.xs}
       gridGap={space.xxs}
-      alignItems="center"
-      overflowY="hidden"
+      overflowY={["auto", "hidden"]}
     >
       <Box>
         <Text textStyle="headline4" pb={space.xs}>
@@ -65,7 +104,11 @@ export default function ReferralModal({
               alignItems="center"
               key={index}
             >
-              <Avatar size={44} />
+              <Image
+                src={step.image}
+                alt={`${index + 1}`}
+                boxProps={{ w: 50 }}
+              />
               <Text>{step.text}</Text>
             </Grid>
           ))}
@@ -73,33 +116,28 @@ export default function ReferralModal({
       </Box>
 
       <Box
-        px={space.xxs}
+        p={space.xxs}
         border={`1px solid ${colors.accent}`}
-        alignSelf="center"
+        alignSelf={["start", "center"]}
       >
-        <Text textStyle="headline6" py={space.xxs}>
+        <Text textStyle="headline6" pb={space.xs}>
           Track your referrals
         </Text>
 
-        <Flex alignItems="center" justifyContent="space-between" pb={space.xxs}>
-          <Text>Referrals who watched streams</Text>
-          <Text>{referralSummary?.total_referrals}</Text>
-        </Flex>
-
-        <Flex alignItems="center" justifyContent="space-between" pb={space.xxs}>
-          <Text>Total Payable</Text>
-          <Text>{referralSummary?.total_payable}</Text>
-        </Flex>
-
-        <Flex alignItems="center" justifyContent="space-between" pb={space.xxs}>
-          <Text>Paid out</Text>
-          <Text>{referralSummary?.paid_out}</Text>
-        </Flex>
-
-        <Flex alignItems="center" justifyContent="space-between" pb={space.xxs}>
-          <Text>Outstanding</Text>
-          <Text>{referralSummary?.outstanding_payment}</Text>
-        </Flex>
+        {referralSummaryData.map(({ title, key, display }) => {
+          return (
+            <Grid
+              pb={space.xxs}
+              gridAutoFlow="column"
+              gridGap={space.xxxs}
+              gridTemplateColumns="1fr auto"
+              key={key}
+            >
+              <Text>{title}</Text>
+              <Text>{display}</Text>
+            </Grid>
+          );
+        })}
       </Box>
     </Modal>
   );
