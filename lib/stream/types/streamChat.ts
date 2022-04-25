b906649112dd8af19b0110a3928508a46d55ec37
sanjeev40 <sanjeev@worknetwork.in>
2022-04-25 11:24:59 +0530

    Add chat action type

--- lib/stream/types/streamChat.ts
+++ lib/stream/types/streamChat.ts
@@ -1,6 +1,13 @@
 export enum ChatMessageType {
   TEXT = 1,
   REACTION = 2,
+  ACTION = 3,
+}
+
+export enum ChatActionType {
+  FOLLOW = 1,
+  REFERRAL = 2,
+  STREAMS = 3,
 }
 
 export interface ChatMessage {
@@ -22,4 +29,5 @@ export interface ChatMessage {
     email: string;
     first_name: string;
   };
+  action: ChatActionType;
 }
