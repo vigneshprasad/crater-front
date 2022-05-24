d00191511c0c21a7b21931965aca2893f74648d3
sanjeev40 <sanjeev@worknetwork.in>
2022-05-24 17:32:29 +0530

    Update styled heading divider component color

--- lib/common/components/objects/StyledHeadingDivider/index.tsx
+++ lib/common/components/objects/StyledHeadingDivider/index.tsx
@@ -1,12 +1,12 @@
 import styled, { useTheme } from "styled-components";
 
-import { Box, Grid, Text } from "../../atoms";
+import { Box, Grid, Text, TextProps } from "../../atoms";
 
 interface IProps {
   label: string;
 }
 
-const StyledText = styled(Text)`
+const StyledText = styled(Text)<TextProps>`
   background-color: ${({ theme }) => theme.colors.primaryLight};
   z-index: 1;
 
@@ -19,8 +19,8 @@ const StyledText = styled(Text)`
     left: -8px;
     right: 8px;
     border-radius: 4px;
-    border: 1px solid ${({ theme }) => theme.colors.accentLight};
-    background-color: ${({ theme }) => theme.colors.accentLight};
+    border: 1px solid ${({ theme }) => theme.colors.accent};
+    background-color: ${({ theme }) => theme.colors.accent};
   }
 `;
 
@@ -38,7 +38,7 @@ export default function StyledHeadingDivider({ label }: IProps): JSX.Element {
         <StyledText
           as="div"
           p={space.xxxs}
-          border={`1px solid ${colors.accentLight}`}
+          border={`1px solid ${colors.accent}`}
           borderRadius={radii.xxxxs}
           textStyle="title"
         >
@@ -46,7 +46,7 @@ export default function StyledHeadingDivider({ label }: IProps): JSX.Element {
         </StyledText>
       </Box>
 
-      <Box h={2} bg={colors.black[0]} />
+      <Box h={2} bg={colors.primaryLight} />
     </Grid>
   );
 }
