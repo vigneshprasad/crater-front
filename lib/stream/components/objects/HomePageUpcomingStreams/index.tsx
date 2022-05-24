c325071ecc55d8c26d6444c299dadf952ff69392
sanjeev40 <sanjeev@worknetwork.in>
2022-05-24 17:52:59 +0530

    Add home page past and upcoming streams component

---
+++ lib/stream/components/objects/HomePageUpcomingStreams/index.tsx
@@ -0,0 +1,87 @@
+import { useTheme } from "styled-components";
+
+import { Flex, Grid, Icon, Shimmer } from "@/common/components/atoms";
+import { Button } from "@/common/components/atoms/v2";
+import StreamCard from "@/community/components/objects/StreamCard";
+import {
+  UpcomingStreamsContext,
+  UpcomingStreamsProvider,
+} from "@/stream/context/UpcomingStreamsContext";
+
+interface IProps {
+  category?: number;
+}
+
+export default function HomePageUpcomingStreams({
+  category,
+}: IProps): JSX.Element {
+  const { space, colors, radii } = useTheme();
+
+  return (
+    <UpcomingStreamsProvider category={category}>
+      <UpcomingStreamsContext.Consumer>
+        {({ upcoming, loading, nextPage, setUpcomingStreamsPage }) => {
+          return (
+            <>
+              <Grid
+                mx={space.xxs}
+                gridTemplateColumns={[
+                  "1fr",
+                  "repeat(auto-fill, minmax(280px, 1fr))",
+                ]}
+                gridGap={space.s}
+              >
+                {!upcoming || loading
+                  ? Array(4)
+                      .fill("")
+                      .map((_, index) => (
+                        <Shimmer
+                          w="100%"
+                          h={180}
+                          borderRadius={radii.xxs}
+                          key={index}
+                        />
+                      ))
+                  : upcoming.map((stream) => (
+                      <StreamCard
+                        stream={stream}
+                        hostSlug={stream.host_detail?.slug}
+                        key={stream.id}
+                      />
+                    ))}
+              </Grid>
+
+              {nextPage && (
+                <Flex
+                  pt={space.s}
+                  mx={space.xxs}
+                  gridGap={space.xxs}
+                  alignItems="center"
+                >
+                  <Flex flex="1" h={2} bg={colors.primaryLight} />
+                  <Button
+                    mx={space.xxxxxs}
+                    flexGrow={0}
+                    variant="pagination"
+                    label="Show more"
+                    onClick={() => setUpcomingStreamsPage((page) => page + 1)}
+                    display="flex"
+                    alignItems="center"
+                    gridGap={space.xxxxxs}
+                  >
+                    <Icon
+                      color={colors.accentLight}
+                      icon="ChevronDown"
+                      pt={2}
+                    />
+                  </Button>
+                  <Flex flex="1" h={2} bg={colors.primaryLight} />
+                </Flex>
+              )}
+            </>
+          );
+        }}
+      </UpcomingStreamsContext.Consumer>
+    </UpcomingStreamsProvider>
+  );
+}
