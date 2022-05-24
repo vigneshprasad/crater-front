32e45ab21352c9a330f9281b2e65305d0be27c81
sanjeev40 <sanjeev@worknetwork.in>
2022-05-24 17:31:30 +0530

    Remove styled heading in home page layout

--- lib/common/components/layouts/v2/HomePageLayout/index.tsx
+++ lib/common/components/layouts/v2/HomePageLayout/index.tsx
@@ -1,9 +1,9 @@
 import { Variants } from "framer-motion";
 import { NextSeoProps } from "next-seo";
 import { PropsWithChildren } from "react";
-import styled, { useTheme } from "styled-components";
+import { useTheme } from "styled-components";
 
-import { AnimatedBox, Box, Text, TextProps } from "@/common/components/atoms";
+import { AnimatedBox, Box, Text } from "@/common/components/atoms";
 import AsideNav from "@/common/components/objects/AsideNav";
 import Page from "@/common/components/objects/Page";
 import { INavKeys } from "@/common/constants/ui.constants";
@@ -23,21 +23,6 @@ const variants: Variants = {
   exit: { opacity: 0, x: 0, y: 10 },
 };
 
-const StyledHeading = styled(Text)<TextProps>`
-  text-align: center;
-  background: linear-gradient(
-    90.47deg,
-    #d5bbff 17.58%,
-    #9db3ff 60.39%,
-    #0d849e 85.38%
-  );
-  text-shadow: 0px 0px 28px rgba(136, 46, 232, 0.8);
-  background-clip: text;
-  text-fill-color: transparent;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
-
 export default function HomePageLayout({
   children,
   seo,
@@ -51,9 +36,9 @@ export default function HomePageLayout({
     <Page seo={seo}>
       <BaseLayout aside={<AsideNav activeTab={activeTab} />} overflowY="auto">
         <Box px={space.xs} py={32}>
-          <StyledHeading textStyle="headline5" maxLines={2}>
+          <Text textStyle="mainHeading" maxLines={2} textAlign="center">
             {heading}
-          </StyledHeading>
+          </Text>
           {subHeading && (
             <Text textStyle="body" textAlign="center" maxLines={2}>
               {subHeading}
