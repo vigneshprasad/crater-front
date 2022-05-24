a82d4ef2e44eb0784ff07c17ef20c74de1319920
sanjeev40 <sanjeev@worknetwork.in>
2022-05-24 17:53:17 +0530

    Update past stream list home component

--- lib/stream/components/objects/PastStreamListHome/index.tsx
+++ lib/stream/components/objects/PastStreamListHome/index.tsx
@@ -1,38 +1,24 @@
-import { useCallback, useRef } from "react";
 import { useTheme } from "styled-components";
 
 import { Grid, Shimmer } from "@/common/components/atoms";
-import { PageRoutes } from "@/common/constants/route.constants";
-import { IPastStreamState } from "@/stream/context/PastStreamContext";
+import { Webinar } from "@/community/types/community";
 
-import PastStreamCard from "../PastStreamCard";
+import PastStreamCard from "../PastStreamCard/v2";
+
+interface IProps {
+  streams?: Webinar[];
+  loading: boolean;
+}
 
 export default function PastStreamListHome({
   streams: pastStreams,
   loading,
-  setPastStreamsPage,
-}: IPastStreamState): JSX.Element {
+}: IProps): JSX.Element {
   const { space, radii } = useTheme();
-
-  const _observer = useRef<IntersectionObserver>();
-
-  const ref = useCallback(
-    (node: HTMLDivElement | null) => {
-      if (loading) return;
-      if (_observer.current) _observer.current.disconnect();
-      _observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          setPastStreamsPage((page) => page + 1);
-        }
-      });
-
-      if (node != null) _observer.current.observe(node);
-    },
-    [_observer, loading, setPastStreamsPage]
-  );
 
   return (
     <Grid
+      mx={space.xxs}
       gridTemplateColumns={["1fr", "repeat(auto-fill, minmax(280px, 1fr))"]}
       gridGap={space.s}
     >
@@ -42,18 +28,8 @@ export default function PastStreamListHome({
             .map((_, index) => (
               <Shimmer w="100%" h={180} borderRadius={radii.xxs} key={index} />
             ))
-        : pastStreams.map((stream, index) => (
-            <PastStreamCard
-              key={stream.id}
-              title={stream.topic_detail.name}
-              href={PageRoutes.streamVideo(stream.id)}
-              image={stream.topic_detail.image}
-              hostImage={stream.host_detail?.photo}
-              hostName={stream.host_detail?.name}
-              time={stream.start}
-              hostSlug={stream.host_detail?.slug}
-              ref={index == pastStreams.length - 1 ? ref : null}
-            />
+        : pastStreams.map((stream) => (
+            <PastStreamCard key={stream.id} stream={stream} />
           ))}
     </Grid>
   );
