bb5a41f6496416b0a76ca3dda19779e9c9b25c7e
Abhishek R <abhishek@worknetwork.in>
2022-06-23 01:22:42 +0530

    added clarity script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ import { SWRConfig } from "swr";
 
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 import AuthModal from "@/auth/components/objects/AuthModal";
 import BasicSignupSheet from "@/auth/components/objects/BasicSignupSheet";
@@ -102,6 +103,15 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
                 <GlobalStyle />
                 <AsideNavProvider>
                   <SystemSocketProvider>
+                    <Script strategy="afterInteractive">
+                      {`
+                        (function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "bmhdbl1977");
+                      `}
+                    </Script>
                     <Component {...pageProps} />
                   </SystemSocketProvider>
                 </AsideNavProvider>
