44f086c333072cf5481583a37b17c313e8b8ce83
sanjeev40 <sanjeev@worknetwork.in>
2022-06-23 13:04:34 +0530

    Update stream question context

--- lib/stream/context/StreamQuestionContext.tsx
+++ lib/stream/context/StreamQuestionContext.tsx
@@ -1,8 +1,15 @@
-import { createContext, PropsWithChildren, useContext, useMemo } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 import { SWRInfiniteResponse, useSWRInfinite } from "swr";
 
 import { API_URL_CONSTANTS } from "@/common/constants/url.constants";
 import { PageResponse } from "@/common/types/api";
+import fetcher from "@/common/utils/fetcher";
 
 import { StreamQuestion } from "../types/stream";
 
@@ -10,6 +17,7 @@ export interface IStreamQuestionState {
   streamQuestions?: StreamQuestion[];
   error?: unknown;
   loading: boolean;
+  nextPage: boolean;
   setStreamQuestionsPage: SWRInfiniteResponse<
     PageResponse<StreamQuestion>,
     unknown
@@ -31,9 +39,10 @@ type IProviderProps = PropsWithChildren<{
 export function StreamQuestionProvider({
   group,
   initial,
-  pageSize = 10,
+  pageSize = 5,
   ...rest
 }: IProviderProps): JSX.Element {
+  const [nextPage, setNextPage] = useState(initial?.next ? true : false);
   const {
     data: streamQuestions,
     error,
@@ -46,6 +55,11 @@ export function StreamQuestionProvider({
 
       return `${API_URL_CONSTANTS.groups.getGroupQuestions}?group=${group}&page=${page}&page_size=${pageSize}`;
     },
+    async (key: string) => {
+      const response = await fetcher<PageResponse<StreamQuestion>>(key);
+      !response.next ? setNextPage(false) : setNextPage(true);
+      return response;
+    },
     {
       initialData: initial ? [initial] : undefined,
     }
@@ -56,10 +70,17 @@ export function StreamQuestionProvider({
       streamQuestions: streamQuestions?.flatMap((page) => page.results),
       error,
       loading: !streamQuestions && !error,
+      nextPage,
       setStreamQuestionsPage,
       mutateStreamQuestionsPage,
     }),
-    [streamQuestions, error, setStreamQuestionsPage, mutateStreamQuestionsPage]
+    [
+      streamQuestions,
+      error,
+      nextPage,
+      setStreamQuestionsPage,
+      mutateStreamQuestionsPage,
+    ]
   );
 
   return <StreamQuestionContext.Provider value={value} {...rest} />;
