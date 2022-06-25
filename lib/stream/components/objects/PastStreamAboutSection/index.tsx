9f0eea1fdb662213708b5b0fb72c692138efaeba
sanjeev40 <sanjeev@worknetwork.in>
2022-06-25 17:13:14 +0530

    Update past stream about section

--- lib/stream/components/objects/PastStreamAboutSection/index.tsx
+++ lib/stream/components/objects/PastStreamAboutSection/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useTheme } from "styled-components";
 
 import { Box, Flex, Icon, Text } from "@/common/components/atoms";
+import { Button } from "@/common/components/atoms/v2";
 import DateTime from "@/common/utils/datetime/DateTime";
 import { Follower, Webinar } from "@/community/types/community";
 
+import ShareStreamModal from "../ShareStreamModal";
 import StreamAboutSection from "../StreamAboutSection";
 
 interface IProps {
@@ -11,6 +14,7 @@ interface IProps {
   followers?: Follower[];
   followersLoading: boolean;
   onFollow: () => void;
+  scrollToForum: () => void;
 }
 
 export default function PastStreamAboutSection({
@@ -18,8 +22,10 @@ export default function PastStreamAboutSection({
   followers,
   followersLoading,
   onFollow,
+  scrollToForum,
 }: IProps): JSX.Element {
   const { space, colors } = useTheme();
+  const [showShareModal, setShowShareModal] = useState(false);
 
   const startTime = DateTime.parse_with_milliseconds(stream.start).toFormat(
     DateTime.DEFAULT_FORMAT
@@ -27,6 +33,12 @@ export default function PastStreamAboutSection({
 
   return (
     <Box>
+      <ShareStreamModal
+        stream={stream}
+        visible={showShareModal}
+        onClose={() => setShowShareModal(false)}
+      />
+
       <Flex pb={space.xxs} flexDirection="row" justifyContent="space-between">
         <Flex gridGap={4} alignItems="center">
           <Icon color={colors.textSecondary} icon="Calendar" size={16} />
@@ -35,24 +47,39 @@ export default function PastStreamAboutSection({
           </Text>
         </Flex>
         <Flex flexDirection="row" gridGap={space.xs}>
-          <Flex justifyContent="center" alignItems="center" gridGap={6}>
-            <Icon
-              icon="QuestionMark"
-              size={20}
-              color={colors.white[0]}
-              fill={true}
-            />
-            <Text textStyle="small" fontWeight={500}>
-              Forum
-            </Text>
-          </Flex>
+          <Button
+            variant="text"
+            label="Forum"
+            textProps={{
+              color: colors.textPrimary,
+            }}
+            prefixElement={
+              <Icon
+                icon="QuestionMark"
+                size={20}
+                color={colors.white[0]}
+                fill={true}
+              />
+            }
+            onClick={() => scrollToForum()}
+          />
 
-          <Flex justifyContent="center" alignItems="center" gridGap={6}>
-            <Icon icon="Share" size={20} color={colors.white[0]} fill={true} />
-            <Text textStyle="small" fontWeight={500}>
-              Share stream
-            </Text>
-          </Flex>
+          <Button
+            variant="text"
+            label="Share stream"
+            textProps={{
+              color: colors.textPrimary,
+            }}
+            prefixElement={
+              <Icon
+                icon="Share"
+                size={20}
+                color={colors.white[0]}
+                fill={true}
+              />
+            }
+            onClick={() => setShowShareModal(true)}
+          />
         </Flex>
       </Flex>
 
