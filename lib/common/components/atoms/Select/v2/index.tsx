c125913117b66bb0a38644c66b05e62a0c846967
sanjeev40 <sanjeev@worknetwork.in>
2022-08-05 12:48:11 +0530

    Add v2 select component

---
+++ lib/common/components/atoms/Select/v2/index.tsx
@@ -0,0 +1,172 @@
+import { useAnimation } from "framer-motion";
+import { useCallback, useEffect, useState } from "react";
+import styled, { useTheme } from "styled-components";
+import useSWR from "swr";
+
+import { AnimatedBox } from "../../Animated";
+import { Icon } from "../../Icon";
+import { Box, Grid, Text } from "../../System";
+
+export type SelectProps<T> = {
+  async?: boolean;
+  items?: T[];
+  dataUrl?: string;
+  value?: unknown;
+  label: string;
+  itemLabelGetter: (val: T) => string;
+  onChange?: (val: unknown) => void;
+  dataTransform?: (val: T) => unknown;
+};
+
+const Container = styled(Box)`
+  position: relative;
+  background: ${({ theme: { colors } }) => colors.primaryBackground};
+  padding: 0.6em 1em;
+  border-radius: ${({ theme: { radii } }) => radii.xxxxs}px;
+  border: 1px solid ${({ theme: { colors } }) => colors.secondaryLight};
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background: ${({ theme: { colors } }) => colors.primaryLight};
+  }
+
+  &:focus {
+    background: ${({ theme: { colors } }) => colors.primaryBackground};
+  }
+`;
+
+export default function Select<T>({
+  value: controlledValue,
+  async = false,
+  dataUrl,
+  itemLabelGetter,
+  label,
+  items: intialItems,
+  onChange,
+  dataTransform,
+}: SelectProps<T>): JSX.Element {
+  const [items, setItems] = useState<T[]>(intialItems ?? []);
+  const [value, setValue] = useState<T>();
+  const [opened, setOpened] = useState(false);
+  const { data: itemData, error } = useSWR<T[]>(dataUrl || null);
+
+  const { colors, space, radii } = useTheme();
+
+  const animate = useAnimation();
+
+  useEffect(() => {
+    if (itemData) {
+      setItems(itemData);
+    }
+  }, [itemData, setItems]);
+
+  const handleItemChange = useCallback(
+    (item: T) => {
+      if (onChange) {
+        const data = dataTransform ? dataTransform(item) : item;
+        onChange(data);
+      }
+    },
+    [onChange, dataTransform]
+  );
+
+  const handleContainerClick = (): void => {
+    setOpened((state) => !state);
+  };
+
+  const handleItemClick = (item: T): void => {
+    if (!controlledValue) {
+      setValue(item);
+    }
+
+    setOpened((state) => !state);
+    handleItemChange(item);
+  };
+
+  useEffect(() => {
+    const variant = opened ? "opened" : "closed";
+    animate.start(variant);
+  }, [opened, animate]);
+
+  useEffect(() => {
+    if (controlledValue && items) {
+      const val = items.filter((item) => {
+        if (dataTransform) {
+          return dataTransform(item) === controlledValue;
+        }
+        return controlledValue === item;
+      })[0];
+
+      if (val) {
+        setValue(val);
+      }
+    }
+  }, [controlledValue, items, dataTransform]);
+
+  if (async) {
+    if (!itemData || error) {
+      return <Box>Loading...</Box>;
+    }
+  }
+
+  return (
+    <Container tabIndex={0} onBlur={() => setOpened(false)}>
+      <Grid
+        zIndex={50}
+        gridTemplateColumns="1fr min-content"
+        alignItems="center"
+        onClick={handleContainerClick}
+      >
+        {value ? (
+          <Text textStyle="body">{itemLabelGetter(value)}</Text>
+        ) : (
+          <Text textStyle="body">{label}</Text>
+        )}
+        <Icon icon="ExpandMore" color={colors.textPrimary} fill />
+      </Grid>
+      <AnimatedBox
+        p={space.xxxxs}
+        w={180}
+        animate={animate}
+        initial="closed"
+        variants={{
+          closed: {
+            display: "none",
+            opacity: 0,
+            top: 0,
+          },
+          opened: {
+            display: "block",
+            opacity: 1,
+            top: "calc(100% + 4px)",
+          },
+        }}
+        zIndex={10}
+        position="absolute"
+        borderRadius={radii.xxxxs}
+        bg={colors.primaryLight}
+        right={0}
+        boxShadow="0px 0px 16px #000000"
+      >
+        {items.map((item) => (
+          <Box
+            my={space.xxxxxs}
+            cursor="pointer"
+            key={itemLabelGetter(item)}
+            px={space.xxxs}
+            py={space.xxxxs}
+            bg={item === value ? colors.secondaryLight : undefined}
+            borderRadius={2}
+            onClick={(event) => {
+              event.stopPropagation();
+              handleItemClick(item);
+            }}
+          >
+            <Text textStyle="body">{itemLabelGetter(item)}</Text>
+          </Box>
+        ))}
+      </AnimatedBox>
+    </Container>
+  );
+}
