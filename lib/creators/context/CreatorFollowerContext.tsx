4c24580ef390fc8c247db5385cc3840ddc4c6778
sanjeev40 <sanjeev@worknetwork.in>
2022-08-09 15:00:54 +0530

    Update creator follower provider

--- lib/creators/context/CreatorFollowerContext.tsx
+++ lib/creators/context/CreatorFollowerContext.tsx
@@ -7,65 +7,85 @@ import {
   useMemo,
   useState,
 } from "react";
-import useSWR from "swr";
+import { SWRInfiniteResponse, useSWRInfinite } from "swr";
 
 import { API_URL_CONSTANTS } from "@/common/constants/url.constants";
 import { PageResponse } from "@/common/types/api";
 import fetcher from "@/common/utils/fetcher";
 import { Follower } from "@/community/types/community";
 
-interface IContext {
-  pageCount: number;
+interface ICreatorFollowersState {
   followers?: Follower[];
+  followerPages?: PageResponse<Follower>[];
   loading: boolean;
   error: unknown;
-  setPage: Dispatch<SetStateAction<number>>;
-  currentPage: number;
+  nextPage: boolean;
+  pageSize: number;
+  setCreatorFollowersPage: SWRInfiniteResponse<
+    PageResponse<Follower>,
+    unknown
+  >["setSize"];
+  setPageSize: Dispatch<SetStateAction<number>>;
 }
 
-export const CreatorFollowerContext = createContext({} as IContext);
+export const CreatorFollowerContext = createContext(
+  {} as ICreatorFollowersState
+);
 
 type IProviderProps = PropsWithChildren<{
   pageSize?: number;
   userId?: string;
-  initial?: Follower[];
+  initial?: PageResponse<Follower>;
 }>;
 
 export function CreatorFollowerProvider({
-  pageSize = 20,
+  pageSize: initialPageSize = 10,
   initial,
   userId,
   ...rest
 }: IProviderProps): JSX.Element {
-  const [pageCount, setPageCount] = useState(0);
-  const [currentPage, setPage] = useState(1);
-  const { data: followers, error } = useSWR<Follower[]>(
-    `${API_URL_CONSTANTS.creator.getCreatorFollowers}?creator__user=${userId}&page=${currentPage}&pageSize=${pageSize}`,
-    async (key: string) => {
-      const res = await fetcher<PageResponse<Follower>>(key);
-      const pageCount = Math.ceil(res.count / pageSize);
-      setPageCount(pageCount);
-      return res.results;
+  const [pageSize, setPageSize] = useState(initialPageSize);
+  const [nextPage, setNextPage] = useState(initial?.next ? true : false);
+
+  const {
+    data: followers,
+    error,
+    setSize: setCreatorFollowersPage,
+  } = useSWRInfinite<PageResponse<Follower>>(
+    (index, previousData) => {
+      const page = index + 1;
+      if (previousData && !previousData.next) return null;
+
+      return `${API_URL_CONSTANTS.creator.getCreatorFollowers}?creator__user=${userId}&page=${page}&page_size=${pageSize}`;
     },
-    { initialData: initial }
+    async (key: string) => {
+      const response = await fetcher<PageResponse<Follower>>(key);
+      !response.next ? setNextPage(false) : setNextPage(true);
+      return response;
+    },
+    {
+      initialData: initial ? [initial] : undefined,
+    }
   );
 
-  const value = useMemo(
+  const value: ICreatorFollowersState = useMemo(
     () => ({
-      followers,
+      followers: followers?.flatMap((page) => page.results),
+      followerPages: followers,
       error,
-      pageCount,
       loading: !followers && !error,
-      setPage,
-      currentPage,
+      nextPage,
+      pageSize,
+      setCreatorFollowersPage,
+      setPageSize,
     }),
-    [followers, error, pageCount, currentPage]
+    [followers, error, nextPage, pageSize, setCreatorFollowersPage, setPageSize]
   );
 
   return <CreatorFollowerContext.Provider value={value} {...rest} />;
 }
 
-export default function useCreatorFollowers(): IContext {
+export default function useCreatorFollowers(): ICreatorFollowersState {
   const context = useContext(CreatorFollowerContext);
 
   if (!context) {
