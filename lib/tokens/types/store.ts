f12d3045515871b183d913d146e67ad6d27d8252
sanjeev40 <sanjeev@worknetwork.in>
2022-09-01 21:02:04 +0530

    Add store types

--- lib/tokens/types/store.ts
+++ lib/tokens/types/store.ts
@@ -21,6 +21,11 @@ export const STORE_ITEMS: IStoreItem[] = [
   },
 ];
 
+export enum RewardSalePaymentType {
+  INR = 1,
+  Learn = 2,
+}
+
 export interface Seller {
   id: number;
   user: string;
@@ -28,6 +33,28 @@ export interface Seller {
   profile_detail: {
     id: number;
     name: string;
-    photo?: string;
+    photo: string | null;
   };
+}
+
+export interface RewardSale {
+  id: number;
+  payment_type: RewardSalePaymentType;
+  price: number;
+  quantity: number;
+  quantity_sold: number;
+  reward: number;
+}
+
+export interface SaleItem {
+  id: number;
+  creator: number;
+  title: string;
+  description?: string;
+  photo: string | null;
+  creator_detail: {
+    id: number;
+    name: string;
+  };
+  reward_sale_details: RewardSale[];
 }
