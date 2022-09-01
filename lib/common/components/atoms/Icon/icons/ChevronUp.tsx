480d6564f4af9fac9ffdb2b9ffb298b8b15ec752
sanjeev40 <sanjeev@worknetwork.in>
2022-09-01 11:53:31 +0530

    Add store related images and icons

---
+++ lib/common/components/atoms/Icon/icons/ChevronUp.tsx
@@ -0,0 +1,20 @@
+export default function ChevronUp(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M5 12.5L10 7.5L15 12.5" />
+    </svg>
+  );
+}
--- public/images/index.tsx
+++ public/images/index.tsx
@@ -1,6 +1,7 @@
 import GifLearnToken3d from "public/images/gif_learn_token.gif";
 import ImageAppBannerStream from "public/images/img_app_banner_livestream.png";
 import ImageAuctionInactive from "public/images/img_auction_inactive.png";
+import BecomeACreator from "public/images/img_become_creator.png";
 import ImageBidAcceptedIcon from "public/images/img_bid_accepted.png";
 import ImageBidRejectedIcon from "public/images/img_bid_rejected.png";
 import ImageBlobBlue from "public/images/img_blob_blue.png";
@@ -21,7 +22,9 @@ import ImageRank2 from "public/images/img_rank_2.png";
 import ImageRank3 from "public/images/img_rank_3.png";
 import ImageRewardBlob from "public/images/img_reward_blob.png";
 import ImageRewardCard from "public/images/img_reward_card.png";
+import Sale from "public/images/img_sale.png";
 import ImageStarryBg from "public/images/img_starry_bg.png";
+import Stream from "public/images/img_stream.png";
 import ImageCraterStream from "public/images/img_stream_crater.png";
 import ImageLearnLoot from "public/images/img_total_loot.png";
 import ImageTv from "public/images/img_tv.png";
@@ -56,6 +59,9 @@ const STATIC_IMAGES = {
   ImageHyfiPlaceholder,
   ImageLearnLoot,
   GifLearnToken3d,
+  BecomeACreator,
+  Stream,
+  Sale,
 };
 
 export default STATIC_IMAGES;
