2f2c116d9d6b0b1b194451cb49de3ca7b9293505
sanjeev40 <sanjeev@worknetwork.in>
2022-09-02 18:00:55 +0530

    Add sale item card component

---
+++ lib/tokens/components/objects/SaleItemCard/index.tsx
@@ -0,0 +1,97 @@
+import STATIC_IMAGES from "public/images";
+import { useTheme } from "styled-components";
+
+import {
+  Avatar,
+  Box,
+  Flex,
+  Icon,
+  Image,
+  Span,
+  Text,
+} from "@/common/components/atoms";
+import { RewardSalePaymentType, SaleItem } from "@/tokens/types/store";
+
+type IProps = {
+  saleItem: SaleItem;
+};
+
+export default function SaleItemCard({ saleItem }: IProps): JSX.Element {
+  const { space, colors, radii } = useTheme();
+
+  const rewardSale = saleItem.reward_sale_details[0];
+  const payWithLearn = rewardSale.payment_type === RewardSalePaymentType.Learn;
+  const quantitySold = rewardSale.quantity - rewardSale.quantity_sold;
+
+  return (
+    <Box
+      maxWidth={278}
+      px={space.xs}
+      pt={space.xs}
+      pb={space.xxs}
+      bg={colors.primaryDark}
+      borderRadius={radii.xs}
+      border={`1px solid ${colors.primaryLight}`}
+    >
+      {saleItem.photo ? (
+        <Image
+          src={saleItem.photo}
+          alt={saleItem.title}
+          objectFit="cover"
+          boxProps={{
+            position: "relative",
+            w: 238,
+            h: 238,
+            borderRadius: radii.s,
+            overflow: "hidden",
+          }}
+        />
+      ) : (
+        <Box pb={space.xxxs}>
+          <Image
+            src={STATIC_IMAGES.ImageDefaultSaleItem}
+            alt={saleItem.title}
+            objectFit="cover"
+            boxProps={{
+              position: "relative",
+              w: 56,
+              h: 56,
+            }}
+          />
+        </Box>
+      )}
+      <Text pt={space.xxs} textStyle="formLabel">
+        {saleItem.title}
+      </Text>
+      <Flex pt={space.xxxs} alignItems="center" gridGap={space.xxxxs}>
+        <Text textStyle="captionLarge" color={colors.textQuartenary}>
+          Price:
+        </Text>
+        {payWithLearn ? (
+          <Flex alignItems="center" gridGap={space.xxxxxs}>
+            <Text textStyle="menu">{rewardSale.price} LEARN</Text>
+            <Icon icon="LearnToken" size={16} />
+          </Flex>
+        ) : (
+          <Text textStyle="menu">₹{rewardSale.price}</Text>
+        )}
+      </Flex>
+      <Flex pt={space.xxxs} alignItems="center" gridGap={space.xxxxs}>
+        <Text textStyle="captionLarge" color={colors.textQuartenary}>
+          Stock Left:
+        </Text>
+        <Text textStyle="menu">{quantitySold === 0 ? "-" : quantitySold}</Text>
+      </Flex>
+      <Flex
+        mt={space.xxxs}
+        pt={space.xxs}
+        borderTop={`1px solid ${colors.secondaryLight}`}
+        alignItems="center"
+        gridGap={space.xxxxs}
+      >
+        <Avatar size={24} />
+        <Text textStyle="captionLarge">{saleItem.creator_detail.name}</Text>
+      </Flex>
+    </Box>
+  );
+}
