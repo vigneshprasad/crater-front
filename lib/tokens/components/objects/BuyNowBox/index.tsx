53d00494355e180ee7842a41d0686071f97ecbc7
sanjeev40 <sanjeev@worknetwork.in>
2022-09-07 00:19:10 +0530

    Add on click for buy now button

--- lib/tokens/components/objects/BuyNowBox/index.tsx
+++ lib/tokens/components/objects/BuyNowBox/index.tsx
@@ -1,6 +1,10 @@
 import { useTheme } from "styled-components";
 
+import { useRouter } from "next/router";
+
 import { RewardSale } from "@/auction/types/sales";
+import useAuth from "@/auth/context/AuthContext";
+import useAuthModal from "@/auth/context/AuthModalContext";
 import { Box } from "@/common/components/atoms";
 import { Button } from "@/common/components/atoms/v2";
 
@@ -12,6 +16,18 @@ type IProps = {
 
 export default function BuyNowBox({ sale }: IProps): JSX.Element {
   const { radii } = useTheme();
+  const router = useRouter();
+  const { user } = useAuth();
+  const { openModal } = useAuthModal();
+
+  const disableBuyNow =
+    user?.pk === sale.reward_detail.creator_detail.user ||
+    sale.is_active === false;
+
+  const openSale = (saleId: number): void => {
+    router.query.sale = `${saleId}`;
+    router.push(router, undefined, { shallow: true });
+  };
 
   return (
     <Box
@@ -28,9 +44,12 @@ export default function BuyNowBox({ sale }: IProps): JSX.Element {
         mt={24}
         w="100%"
         minHeight={44}
-        label="Buy Now 🎉 (Coming Soon)"
+        label="Buy Now 🎉"
         textProps={{ fontSize: "1.6rem" }}
-        disabled={true}
+        onClick={() => {
+          user ? openSale(sale.id) : openModal();
+        }}
+        disabled={disableBuyNow}
       />
     </Box>
   );
