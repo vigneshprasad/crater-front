176e65b8a967d97312a5f2264c23d96dee465f65
sanjeev40 <sanjeev@worknetwork.in>
2022-09-08 21:11:09 +0530

    Use current color for fill in icons

--- lib/common/components/atoms/Icon/icons/Profile.tsx
+++ lib/common/components/atoms/Icon/icons/Profile.tsx
@@ -12,7 +12,7 @@ export default function Profile(
     >
       <path
         d="M16 21H0v-2a5 5 0 015-5h6a5 5 0 015 5v2zm-8-9A6 6 0 118-.001 6 6 0 018 12z"
-        fill="#000"
+        fill="currentColor"
       />
     </svg>
   );
