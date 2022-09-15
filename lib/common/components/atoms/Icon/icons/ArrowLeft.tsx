9ab8c987174cd6483bebac50a692c97343c88599
sanjeev40 <sanjeev@worknetwork.in>
2022-09-15 22:20:37 +0530

    Update arrow left icon and svg

--- lib/common/components/atoms/Icon/icons/ArrowLeft.tsx
+++ lib/common/components/atoms/Icon/icons/ArrowLeft.tsx
@@ -3,14 +3,17 @@ export default function ArrowLeft(
 ): JSX.Element {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="1em"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
-      width="1em"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M24 0v24H0V0h24z" fill="none" opacity={0.87} />
-      <path d="M14 7l-5 5 5 5V7z" />
+      <path
+        d="M7.828 13.0007L13.192 18.3647L11.778 19.7787L4 12.0007L11.778 4.22266L13.192 5.63666L7.828 11.0007L20 11.0007L20 13.0007L7.828 13.0007Z"
+        fill="currentColor"
+      />
     </svg>
   );
 }
