d4a7ddc0d16dea57c22358e9e423db02f20df72e
Abhishek R <abhishek@worknetwork.in>
2022-09-30 19:58:12 +0530

    Icon fixes and added new fields in account form

--- lib/common/components/atoms/Icon/icons/IcNotification.tsx
+++ lib/common/components/atoms/Icon/icons/IcNotification.tsx
@@ -12,7 +12,7 @@ export default function IcNotification(
     >
       <path
         d="M20.22 16.62c0 .85-.69 1.54-1.54 1.54H5.32a1.54 1.54 0 010-3.08h.51V9.94c0-3.4 2.76-6.17 6.17-6.17 1.7 0 3.24.69 4.36 1.81a6.11 6.11 0 011.81 4.36v5.14h.51c.85 0 1.54.69 1.54 1.54zM12 3.78V2.75M15.08 18.17c0 1.71-1.38 3.08-3.08 3.08-1.7 0-3.08-1.38-3.08-3.08h6.16z"
-        stroke="#000"
+        stroke="currentColor"
         strokeWidth={1.5}
         strokeLinecap="round"
         strokeLinejoin="round"
