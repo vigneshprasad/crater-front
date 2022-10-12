fc7292add9157ed9584ed9e8cf36e45503506518
Abhishek R <abhishek@worknetwork.in>
2022-10-13 03:59:24 +0530

    Fixed overflowing content in Dyte player

--- lib/stream/components/objects/StreamDytePlayer/index.tsx
+++ lib/stream/components/objects/StreamDytePlayer/index.tsx
@@ -101,7 +101,14 @@ export default function StreamDytePlayer({
         }
 
         return (
-          <Box position="absolute" top={0} left={0} right={0} bottom={0}>
+          <Box
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            overflow="hidden"
+          >
             <DyteMeeting
               uiConfig={{
                 header: false,
