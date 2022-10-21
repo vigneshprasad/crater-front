f574b8cf24a43c66b1be1ec7a3166905aa19d43b
sanjeev40 <sanjeev@worknetwork.in>
2022-10-21 14:15:46 +0530

    Add stickers

--- public/images/emoji/index.ts
+++ public/images/emoji/index.ts
@@ -16,9 +16,11 @@ import Hacker from "public/images/emoji/img_hacker.png";
 import HandOverHangOver from "public/images/emoji/img_handover_hang_over.png";
 import HandOverTorch from "public/images/emoji/img_handover_torch.png";
 import Happy from "public/images/emoji/img_happy.png";
+import HeemanshuMetaverse from "public/images/emoji/img_heemanshu_metaverse.png";
 import HelloHacker from "public/images/emoji/img_hello_hacker.png";
 import HelloWorld from "public/images/emoji/img_hello_world.png";
 import Hi from "public/images/emoji/img_hi.png";
+import HimanshuSachanBirthday from "public/images/emoji/img_himanshu_sachan_birthday.png";
 import ImIn from "public/images/emoji/img_i_am_in.png";
 import Interesting from "public/images/emoji/img_interesting.png";
 import IQ220 from "public/images/emoji/img_iq_220.png";
@@ -106,6 +108,8 @@ const EMOJI_IMAGE_LIST = {
   StackOverflow,
   Supremacy,
   YouveBeenHacked,
+  HeemanshuMetaverse,
+  HimanshuSachanBirthday,
 };
 
 export type EmojiKeys = keyof typeof EMOJI_IMAGE_LIST;
