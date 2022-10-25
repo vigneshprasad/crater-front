118a9d0351b2f9c4e3512e85c74775fc7fc256b5
sanjeev40 <sanjeev@worknetwork.in>
2022-10-25 18:35:23 +0530

    Add design landing page

---
+++ pages/join-design/index.tsx
@@ -0,0 +1,37 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+import STATIC_IMAGES from "public/images";
+
+import dynamic from "next/dynamic";
+
+import { PageRoutes } from "@/common/constants/route.constants";
+
+const LandingPage = dynamic(
+  () => import("@/auth/components/pages/LandingPage")
+);
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session?.user) {
+    return {
+      redirect: {
+        destination: PageRoutes.category("design"),
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export default function Landing(): JSX.Element {
+  return (
+    <LandingPage
+      primaryHeading="Design & Art Creators"
+      secondaryHeading="Watch live streams on UI/UX design, Art, Sketching and more on Crater today. 120,000 learners have already joined!"
+      image={STATIC_IMAGES.ImageDesignLandingPage}
+    />
+  );
+}
